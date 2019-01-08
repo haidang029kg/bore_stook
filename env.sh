1a8a8ee72452971e7cafa19da814b83886261423
thinh <thinhhoang104@gmail.com>
2019-01-08 12:09:32 +0700

    add addDummy

---
+++ env.sh
@@ -0,0 +1,8 @@
+export MYSQL_HOST=localhost
+export MYSQL_USER=flask
+export MYSQL_PASSWORD=Flask_123
+export MYSQL_DB=borestook
+export SECRET_KEY=1356d24c50414b44221cbc80ac0bf45a
+export MAIL_USERNAME=vuttan1995@gmail.com
+export MAIL_PASSWORD=01664292129
+export CLOUD_SQL_INSTANCES=final-thesis-100496:asia-east2:borestook
